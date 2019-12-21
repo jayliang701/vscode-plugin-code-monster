570b9d72bda7d327174c93c969e1712062f5df61
jayliang701 <156070304@qq.com>
2019-12-21 16:10:45 +0800

    update

--- src/test-menu-when.js
+++ src/test-menu-when.js
@@ -1,16 +1,55 @@
 const vscode = require('vscode');
-module.exports = function(context) {
-    context.subscriptions.push(vscode.commands.registerCommand('extension.chooseTemplate', async () => {
-        // vscode.window.showInformationMessage(`你点我干啥，我长得很帅吗？`);
-        const result = await window.showInputBox({
-            value: 'abcdef',
-            valueSelection: [2, 4],
-            placeHolder: 'For example: fedcba. But not: 123',
-            validateInput: text => {
-                window.showInformationMessage(`Validating: ${text}`);
-                return text === '123' ? 'Not 123!' : null;
-            }
+
+const langs = {
+    react: async () => {
+        let items = [
+            { description:'Standard React Component. add -scss/css/less to enable style file.', label:'comp' },
+            { description:'Function style React Component. add -scss/css/less to enable style file.', label:'fcomp' }
+        ];
+        let selectedTemplate = await vscode.window.showQuickPick(items, { matchOnDetail: true, matchOnDescription: true });
+        
+        const result = await vscode.window.showInputBox({
+            value: '',
+            valueSelection: [0, 0],
+            placeHolder: '',
         });
-        window.showInformationMessage(`Got: ${result}`);
+        const args = result.replace(/\s+/img, ' ').split(' ');
+        
+        return [
+            'react', 
+            selectedTemplate.label, 
+            ...args
+        ];
+    }
+};
+
+let items = [
+    { description: 'React/ReactNative code templates', label: 'react' }
+];
+
+const showView = async (context) => {
+    let selectedLang = await vscode.window.showQuickPick(items, { matchOnDetail: true, matchOnDescription: true });
+    let args = await langs[selectedLang.label](context);
+    console.log(args);
+}
+
+module.exports = function (context) {
+    context.subscriptions.push(vscode.commands.registerCommand('extension.chooseTemplate', async () => {
+        // const result = await vscode.window.showInputBox({
+        //     value: '',
+        //     valueSelection: [0, 0],
+        //     placeHolder: 'commands:\r\nreact comp MyComponent -scss',
+        //     validateInput: text => {
+        //         window.showInformationMessage(`Validating: ${text}`);
+        //         return text === '123' ? 'Not 123!' : null;
+        //     }
+        // });
+        // vscode.window.showInformationMessage(`Got: ${result}`);
+
+        try {
+            await showView(context);
+        } catch (err) {
+            console.error(err);
+        }
     }));
 };
