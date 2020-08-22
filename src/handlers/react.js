0e8b2cc4b34cf8942db59384072d53514b661a0d
jayliang701 <156070304@qq.com>
2020-08-22 14:16:51 +0800

    增加 typescript 组件模板

--- src/handlers/react.js
+++ src/handlers/react.js
@@ -4,7 +4,8 @@ const vscode = require('vscode');
 exports.exec = async () => {
     let items = [
         { description:'Standard React Component. add -scss/css/less to enable style file. add -mobx to use mobx state.', label:'comp' },
-        { description:'Function style React Component. add -scss/css/less to enable style file. add -mobx to use mobx state.', label:'fcomp' }
+        { description:'Function style React Component. add -scss/css/less to enable style file. add -mobx to use mobx state. add -index to generate index.js file', label:'fcomp' },
+        { description:'Function style React Component (Typescript). add -scss/css/less to enable style file. add -mobx to use mobx state. add -index to generate index.tsx file', label:'fcompts' }
     ];
     let selectedTemplate = await vscode.window.showQuickPick(items, { matchOnDetail: true, matchOnDescription: true });
     if (!selectedTemplate) return null;
